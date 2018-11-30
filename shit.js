865fd7fbec100a5653ee2d97a7df38fb0ebd57ff
agrajak <korbots@gmail.com>
2018-11-30 17:38:04 +0900

    퍼즈 기능 추가

--- shit.js
+++ shit.js
@@ -1,18 +1,27 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var shitSpeed = 1;
-var fps = 30;
-var t = 0;
+
+var shitSpeed = 1; // 똥 속도 배율
+var fps = 30; // 화면 주사율
+var t = 0; // 시간(clock)
 var score = 0;
+
+// 플레이어 상태
 var paddleHeight = 10;
 var paddleWidth = 30;
-var paddleX = (canvas.width-paddleWidth)/2;
+var paddleX;
+
 var rightPressed = false;
 var leftPressed = false;
 var shits = []
+
+const PLAYING = 0
+const PAUSE = 1
+const MENU = 2
+const DEAD = 3
+
+var status = 0
+
 var image = new Image();		/* 이미지 객체 생성 */
 image.src = "ddong.png";		/* 이미지 파일 이름 설정 */
 
@@ -28,6 +37,14 @@ function keyDownHandler(e) {
     else if(e.keyCode == 37) {
         leftPressed = true;
     }
+    if(e.keyCode == 80) { // P키
+        if(status == PLAYING) status = PAUSE
+        else status = PLAYING
+    }
+    if(e.keyCode == 78){ // N키
+        reset()
+    }
+
 }
 function keyUpHandler(e) {
     if(e.keyCode == 39) {
@@ -49,19 +66,22 @@ function makeShit(){
   shits.push({x: Math.floor(Math.random() * canvas.width), y: 0, t:t})
 }
 function reset(){
-    t = 0
+    t = score = 0
     shits = []
-    score = 0
+    status = PLAYING
+    paddleX = (canvas.width-paddleWidth)/2
 }
 function drawShit(){
   shits.forEach((shit, index, o)=>{
-    // 똥이 화면을 벗어난다면
     shit.y += (t-shit.t)*shitSpeed;
+    // 똥에 맞았을때
     if(Math.abs(shit.y-canvas.height) < paddleHeight/2 && Math.abs(shit.x-paddleX) < paddleWidth/2){
-        alert('최종 점수 : '+score)
-        reset()
+        alert('최종 점수 : '+score+', 다시 시작하려면 N키를 누르시오')
+        status = DEAD
+        return
     }
 
+    // 똥이 화면을 벗어난다면
     if(shit.y > canvas.height){
       o.splice(index, 1)
       score += 20
@@ -73,8 +93,12 @@ function drawShit(){
   })
 }
 function draw() {
+    if(status != PLAYING){
+        return
+    }
     // draw 는 1000/fps(ms) 마다 실행된다.
     // shitInterval(ms) 마다 될라면...
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPerson();
     if(t*1000/fps%shitInterval == 0){
@@ -82,11 +106,15 @@ function draw() {
     }
     drawShit();
 
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    if(rightPressed) {
         paddleX += 7;
+        if(paddleX > canvas.width)
+            paddleX = 0
     }
-    else if(leftPressed && paddleX > 0) {
+    else if(leftPressed) {
         paddleX -= 7;
+        if(paddleX < 0)
+            paddleX = canvas.width - paddleWidth
     }
   
     t++;
